840f54458d48c16747057ebfb3645d80c7407d48
Chris Clark <forclarkemail@gmail.com>
2018-01-06 08:22:28 -0500

    callback bug update

--- app/config/auth.js
+++ app/config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
 	'githubAuth': {
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': 'http://127.0.0.1:8080/' + 'auth/github/callback'
+		'callbackURL': process.env.APP_URL + 'auth/github/callback'
 	}
 };
